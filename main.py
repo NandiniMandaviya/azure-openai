ea5918108f26e65700fb776e3d2da9cc7b2a7c79
Nandini Mandaviya <122160723+NandiniMandaviya@users.noreply.github.com>
2026-08-07 18:36:47 +0530

    Structured project files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from parsing import run_parse_and_chunk
-from creating_v_db import run_add_to_chroma_db
+from src.components.parsing import run_parse_and_chunk
+from src.components.creating_v_db import run_add_to_chroma_db
 
 if __name__ == "__main__":
     run_parse_and_chunk()
