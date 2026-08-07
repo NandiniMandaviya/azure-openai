b8155cfc865f922d43e9218e10c995e4a9ac274f
Nandini Mandaviya <122160723+NandiniMandaviya@users.noreply.github.com>
2026-08-07 14:50:08 +0530

    Calling functions from main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from utils.parsing_utils import process_text_files
+
 url_filename_mapping = {
     # L&T Corporate
     "https://www.larsentoubro.com/corporate/about-lt-group/overview/":
@@ -46,23 +48,25 @@ url_filename_mapping = {
 
     "https://www.larsentoubro.com/corporate/careers/re-entry-career-re-entry-for-women/":
         "LNT_Careers_Re_entry_for_Women.txt",
-}
 
-pdf_url_filename_mapping = {
     # L&T Annual Reports
-        # Annual Reports
+            # Annual Reports
     "https://annualreview.larsentoubro.com/download/L&T-Annual-Review-2024.pdf":
-        "LT_Annual_Review_2024.pdf",
-
+            "LT_Annual_Review_2024.pdf",
+    
     "https://annualreview.larsentoubro.com/download/L&T-Annual-Review-2023.pdf":
-        "LT_Annual_Review_2023.pdf",
-
+            "LT_Annual_Review_2023.pdf",
+    
     "https://annualreview.larsentoubro.com/download/L&T-Annual-Review-2022.pdf":
-        "LT_Annual_Review_2022.pdf",
-
+            "LT_Annual_Review_2022.pdf",
+    
     "https://annualreview.larsentoubro.com/download/L&T-Annual-Review-2021.pdf":
-        "LT_Annual_Review_2021.pdf",
-
+            "LT_Annual_Review_2021.pdf",
+    
     "https://annualreview.larsentoubro.com/download/L&T-Annual-Review-2020.pdf":
-        "LT_Annual_Review_2020.pdf",
+            "LT_Annual_Review_2020.pdf",
 }
+
+
+if __name__ == "__main__":
+    process_text_files(url_filename_mapping)
